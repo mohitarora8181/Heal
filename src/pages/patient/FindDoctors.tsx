befae292b1cc78f3fd3b33e931a8b5af6a0f264f
Mohit Arora <mannyisha808@gmail.com>
2025-05-25 16:49:31 +0530

    fix time slots

--- src/pages/patient/FindDoctors.tsx
+++ src/pages/patient/FindDoctors.tsx
@@ -210,7 +210,7 @@ export const FindDoctors = () => {
     }
 
     // Generate time slots from 9 AM to 5 PM
-    for (let hour = 9; hour < 17; hour++) {
+    for (let hour = 9; hour < 20; hour++) {
       for (let minute of [0, 30]) {
         // Skip times in the past for today
         if (isToday && (hour < startHour || (hour === startHour && minute < startMinute))) {
